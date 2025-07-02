96d1c3c5cf60eedad978e522d5a470bf74ca96d3
Alexey B <ab2135@example.com>
2025-07-02 20:46:08 +0000

    fix linting errors

--- server/application.py
+++ server/application.py
@@ -1,12 +1,16 @@
+"""My Server"""
 import http.server
 import socketserver
 
 PORT = 8000
 
 class TestMe():
+    """Class for tests"""
     def take_five(self):
+        """returns 5"""
         return 5
     def port(self):
+        """returns port"""
         return PORT
 
 if __name__ == '__main__':
